903dae331366daf6f80c071688c8010a527ee1de
vinithapilli <vinithapilli12@gmail.com>
2024-09-11 22:28:10 +0530

    msg

--- java-basics/index.js
+++ java-basics/index.js
@@ -37,3 +37,83 @@ function greet(name, lastName){
     console.log('hello ' + name +' ' + lastName);
 }
 greet('Vinitha', 'pilli');
+
+//arthematic operator
+let x = 10;
+let y = 3;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y);
+
+//increment 
+console.log(++x);
+
+//decrement
+console.log(--x);
+
+//assignment operator
+ let z = 10;
+ console.log(z);
+
+ z += 5;
+ console.log(z);
+
+ z *= 5;
+ console.log(z);
+
+ //comparision operator
+ let a = 1;
+ console.log(a > 0);
+ console.log(a >= 1);
+ console.log(a < 1);
+ console.log(a <= 1);
+
+ //equality
+ console.log(a === 1);
+ console.log(a !== 1);
+
+ //this equality does not care about type it checks the values in it
+ console.log(a == 1);
+ console.log("1" == 1);
+
+ //ternary operator
+ //if a flowervendor has a more than 100 flowers
+ //they are 'rose' flowers, otherwise,
+ //they are 'lily' flowers.
+ let flowers = 95;
+ let type0 = flowers > 100 ? 'rose' : 'lily';
+ console.log(type0);
+
+ let type1 = flowers < 100 ? 'rose' : 'lily';
+ console.log(type1);
+
+ //logical operators
+ //And operator
+ //let highIncome = true;
+ //let goodCreditScore = true;
+ //let eligibleForLoan = highIncome && goodCreditScore;
+ //console.log(eligibleForLoan);
+
+ //Or operator
+ //let highIncome = false;
+//let goodCreditScore = false;
+//let eligibleForLoan = highIncome || goodCreditScore;
+//console.log(eligibleForLoan);
+
+//Not operator
+//let applicationRefund = !eligibleForLoan;
+//console.log(applicationRefund);
+
+
+
+//bitwise operator
+const readPermission = 4;
+const writePermission = 2;
+const executePermission = 1;
+
+let myPermission = 0;
+myPermission = myPermission | writePermission;
+
