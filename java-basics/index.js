70cf212565a9a537c828b044713d572f2ef8eebb
vinithapilli <vinithapilli12@gmail.com>
2024-09-12 00:13:36 +0530

    msg

--- java-basics/index.js
+++ java-basics/index.js
@@ -106,3 +106,15 @@ console.log(eligibleForLoan);
 //Not operator
 let applicationRefund = !eligibleForLoan;
 console.log(applicationRefund);
+
+
+const readPermission = 4;
+const writePermission = 2;
+const executePermission = 1;
+
+let myPermission = 0;
+myPermission = myPermission | writePermission;
+console.log(myPermission);
+
+let message = (myPermission & readPermission) ? 'yes' : 'no';
+console.log(message);
