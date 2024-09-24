19b46be3b74db3420157b19f5f12cb9b7e3cf74e
vinithapilli <vinithapilli12@gmail.com>
2024-09-24 14:33:05 +0530

    first msg

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-//bit-wise operator
-//read ,write ,execute
-//00000100
-//00000010
-//00000001
+let today = new Date();
+let dayIndex = today.getDay();
 
-const readPermission = 4;
-const writePermission = 2;
-const executePermission = 1;
 
-let myPermission = 0;
-myPermission = myPermission | writePermission;
-console.log(myPermission);
-
-let message = (myPermission & readPermission) ? 'yes' : 'no';
+ if (dayIndex === 0) {
+    console.log('sunday');
+}
+ else if (dayIndex === 1) {
+    console.log('monday');
+} 
+else if (dayIndex === 2) {
+    console.log('tuesday');
+} 
+else if (dayIndex === 3) {
+    console.log('wednesday');
+}
+ else if (dayIndex === 4) {
+    console.log('thursday');
+} 
+else if (dayIndex === 5) {
+    console.log('friday');
+}
+else if(dayIndex === 6){
+ console.log('saturday');
+}
