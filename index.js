cd35f980f1a83448db87d6853d9250e6307059f1
vinithapilli <vinithapilli12@gmail.com>
2024-09-25 22:41:53 +0530

    mdfkh

--- index.js
+++ index.js
@@ -23,3 +23,11 @@ else if (dayIndex === 5) {
 else if(dayIndex === 6){
  console.log('saturday');
 }
+
+//numbers
+const number = [3, 4];
+
+number.push(5, 6);
+
+number.unshift(1, 2);
+console.log(number);
