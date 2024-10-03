34471c7858464005a0858de30e53db44e159b1a7
vinithapilli <vinithapilli12@gmail.com>
2024-10-03 19:40:10 +0530

    ehrbj

--- java-assignment1/index.js
+++ java-assignment1/index.js
@@ -6,4 +6,40 @@ function factorial(n) {
 }
     console.log("Factorial of 6: ",factorial(6));
 
-    console.log("Fctorial of 7: ",factorial(-7));
+    console.log("Factorial of 7: ",factorial(-7));
+
+    // If a string is a palindrome or not
+
+    function isPalindrome(str) {
+        let cleanedStr = str.toLowerCase().replace(/[^a-z 0-9]/g, ''); // Remove non-alphanumeric characters and convert to lowercase
+        let reversedStr = cleanedStr.split('').reverse().join(''); // Reverse the string
+        return cleanedStr === reversedStr;
+    }
+    
+    let word = "Wine";
+     console.log(`${word} is ${isPalindrome(word) ? '' : 'not '}a palindrome.`);
+
+
+     //simple calculator
+     function calculator(num1, num2, operation){
+        let result;
+        switch (operation){
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                result = 'Invalid operation';
+        }
+        return result;
+     }
+
+     console.log("10 + 5 =", calculator(10, 5, '-'));
